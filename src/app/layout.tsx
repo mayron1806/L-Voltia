89c8cb5788fbadd1f2612f1724e6ffe4812232e0
mayron <mayronfernandes01@gmail.com>
2024-08-02 16:29:16 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,7 @@
-import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
-
-const metadataWhatsapp: Metadata = {
-  title: "Zoldy / Whatsapp com super poderes",
-  description: "Transforme seu WhatsApp em uma máquina de vendas 100% automatizada e deixe seus concorrentes para trás!",
-};
-const metadataWeb: Metadata = {
-  title: "Zoldy / Seu chat inteligente",
-  description: "Transforme seu WhatsApp em uma máquina de vendas 100% automatizada e deixe seus concorrentes para trás!",
-};
-export async function generateMetadata({ searchParams }: { searchParams: { template?: 'whatsapp' | 'web' }}) {
-  if(!searchParams?.template) return metadataWeb;
-  if(searchParams.template === 'whatsapp') return metadataWhatsapp;
-  return metadataWeb;
-}
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
 import HomeWeb from "@/components/pages/home-web";
 import HomeWhatsapp from "@/components/pages/home-whatsapp";
+import { Metadata } from "next";
+
+const metadataWhatsapp: Metadata = {
+  title: "Zoldy / Whatsapp com super poderes",
+  description: "Transforme seu WhatsApp em uma máquina de vendas 100% automatizada e deixe seus concorrentes para trás!",
+  openGraph: {
+    type: 'website',
+    images: [{ url: '/logo.png' }],
+  },
+  twitter: {
+    images: [{ url: '/logo.png' }],
+  }
+};
+const metadataWeb: Metadata = {
+  title: "Zoldy / Automatize seu E-commerce",
+  description: "Aumente as vendas e a satisfação dos seus clientes com o nosso chat 100% automatizado!",
+  openGraph: {
+    type: 'website',
+    images: [{ url: '/logo.png' }],
+  },
+  twitter: {
+    images: [{ url: '/logo.png' }],
+  }
+};
+export async function generateMetadata({ searchParams }: { searchParams: { template?: string }}) {
+  if(!searchParams?.template) return metadataWeb;
+  if(searchParams.template === 'whatsapp') return metadataWhatsapp;
+  return metadataWeb;
+}
 
 const Home = ({ searchParams }: {searchParams: { template?: 'whatsapp' | 'web' }}) => {
   return searchParams.template === 'whatsapp' ? <HomeWhatsapp /> : <HomeWeb />;
